c8942badfaf5139ddb10f0920bfe83c4d11953c3
Duc Anh Le <alexdal1992@gmail.com>
2023-01-25 14:23:42 +0200

    refactor Router & FilterField components

---
+++ src/components/Router/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../../pages/Home";
+import Grouping from "../../pages/Grouping";
+import Filtering from '../../pages/Filtering';
+import Sorting from "../../pages/Sorting";
+import Subline from "../../pages/Subline";
+import Editing from "../../pages/Editing";
+
+const Router = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/grouping/" element={<Grouping />} />
+    <Route path="/filtering/" element={<Filtering />} />
+    <Route path="/sorting/" element={<Sorting />} />
+    <Route path="/subline/" element={<Subline />} />
+    <Route path="/editing/" element={<Editing />} />
+  </Routes>
+);
+
+export default Router;
